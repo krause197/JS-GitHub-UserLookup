77540c6ff774e57ab8ff34e40b7879458f1f8591
Eric Krause <krause197@gmail.com>
2016-10-20 10:26:31 -0700

    Update user and user-interface files to accept form input and add new getDetails method

--- js/user-interface.js
+++ js/user-interface.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
   var newSearch = new Search();
   $('#searchButton').click(function(event) {
     event.preventDefault();
-    newSearch.getRepos();
+    var username = $('#username').val();
+    newSearch.getDetails(username);
+    newSearch.getRepos(username);
   });
 });
--- js/user.js
+++ js/user.js
@@ -4,9 +4,21 @@ function Search() {
 
 }
 
-Search.prototype.getRepos = function () {
-  $.get('https://api.github.com/users/daneden?access_token=' +apiKey).then(function(response){
+Search.prototype.getDetails = function (username) {
+  $.get('https://api.github.com/users/'+username+'?access_token='+apiKey).then(function(response) {
     console.log(response);
+    console.log(response.name);
+  }).fail(function(error) {
+    console.log(error.responseJSON.message);
+  });
+};
+
+Search.prototype.getRepos = function (username) {
+  $.get('https://api.github.com/users/'+username+'/repos?access_token=' +apiKey).then(function(response){
+    console.log(response);
+    for (var repo of response) {
+      console.log(repo.name);
+    }
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
